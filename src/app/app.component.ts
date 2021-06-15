416d2ae7e02d17cd63349bce11c552329c58e7cc
Shamant Vijayakumar Gudigenavar --CNTR <ShamantVijayakumar_Gudigenavar@contractor.amat.com>
2021-06-15 11:03:26 +0530

    demo code added

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component,OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'demos';
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+    //Swal.fire('Hello world!');
+    //Swal.fire('Oops...', 'Something went wrong!', 'error');
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this imaginary file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+      // For more information about handling dismissals please visit
+      // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    });
+  }
+
+}
